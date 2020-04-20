92fd965d16b999c82755f7fe57ffa5167d6e620a
dkeska <keskadaniel@gmail.com>
2020-04-20 10:49:39 +0200

    add necessary classes

---
+++ src/main/java/pl/kesco/myfarmer/MyFarmerApplication.java
@@ -0,0 +1,13 @@
+package pl.kesco.myfarmer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFarmerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFarmerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/kesco/myfarmer/MyFarmerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kesco.myfarmer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFarmerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
